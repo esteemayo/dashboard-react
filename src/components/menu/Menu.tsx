cf05451759151b3bbcb0d21c00a86977543f0381
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-18 20:03:45 +0100

    feat: add markup

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,7 +1,34 @@
+import { Link } from 'react-router-dom';
+
 import './menu.scss';
 
 const Menu = () => {
-  return <aside>Menu</aside>;
+  return (
+    <aside>
+      <div className='item'>
+        <span className='title'>Main</span>
+        <Link to='/'>
+          <img src='/home.svg' alt='' />
+          <span className='listItemTitle'>Home</span>
+        </Link>
+        <Link to='/'>
+          <img src='/profile.svg' alt='' />
+          <span className='listItemTitle'>Profile</span>
+        </Link>
+      </div>
+      <div className='item'>
+        <span className='title'>Main</span>
+        <Link to='/'>
+          <img src='/home.svg' alt='' />
+          <span className='listItemTitle'>Home</span>
+        </Link>
+        <Link to='/'>
+          <img src='/profile.svg' alt='' />
+          <span className='listItemTitle'>Profile</span>
+        </Link>
+      </div>
+    </aside>
+  );
 };
 
 export default Menu;
