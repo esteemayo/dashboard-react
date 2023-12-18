3a142c98697ed20a2fc1561363866671f6595a5a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-18 18:05:13 +0100

    feat: add components

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
+import Navbar from '../navbar/Navbar';
+import Menu from '../menu/Menu';
+import Footer from '../footer/Footer';
+
 const Layout = () => {
   return (
     <main className='main'>
+      <Navbar />
+      <Menu />
       <Outlet />
+      <Footer />
     </main>
   );
 };
