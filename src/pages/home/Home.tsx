63a9c70bd23be515fe22792aa49ed221a2bc5be3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-19 00:00:01 +0100

    feat: add markup

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,19 @@
 import './home.scss';
 
 const Home = () => {
-  return <div className='container'>Home</div>;
+  return (
+    <section className='container'>
+      <div className='box box-1'>Box 1</div>
+      <div className='box box-2'>Box 2</div>
+      <div className='box box-3'>Box 3</div>
+      <div className='box box-4'>Box 4</div>
+      <div className='box box-5'>Box 5</div>
+      <div className='box box-6'>Box 6</div>
+      <div className='box box-7'>Box 7</div>
+      <div className='box box-8'>Box 8</div>
+      <div className='box box-9'>Box 9</div>
+    </section>
+  );
 };
 
 export default Home;
