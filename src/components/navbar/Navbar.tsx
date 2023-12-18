9632d83e151a4e42ffff165dbf9688cfe346223d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-18 18:51:23 +0100

    feat: add styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ const Navbar = () => {
     <nav className='navbar'>
       <div className='logo'>
         <img src='logo.svg' alt='logo' />
+        <span>Admin</span>
       </div>
       <div className='icons'>
         <img src='/search.svg' alt='search icon' className='icon' />
