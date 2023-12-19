b23f0b1d66a84796afd14d5794a91d9ecca21c9f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-19 21:29:10 +0100

    feat: add DataTable component

---
+++ src/components/dataTable/DataTable.tsx
@@ -0,0 +1,8 @@
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+
+const DataTable = () => {
+  return <div className='wrapper'>DataTable</div>;
+};
+
+export default DataTable;
