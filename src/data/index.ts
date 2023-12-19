84e8dc707ce3259089fdd619f517a727fc130eb4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-19 20:58:05 +0100

    feat: add areaChartData

--- src/data/index.ts
+++ src/data/index.ts
@@ -332,6 +332,51 @@ export const pieChartData = [
   },
 ];
 
+export const areaChartData = [
+  {
+    name: 'Sun',
+    books: 4000,
+    clothes: 2400,
+    electronic: 2400,
+  },
+  {
+    name: 'Mon',
+    books: 3000,
+    clothes: 1398,
+    electronic: 2210,
+  },
+  {
+    name: 'Tue',
+    books: 2000,
+    clothes: 9800,
+    electronic: 2290,
+  },
+  {
+    name: 'Wed',
+    books: 2780,
+    clothes: 3908,
+    electronic: 2000,
+  },
+  {
+    name: 'Thu',
+    books: 1890,
+    clothes: 4800,
+    electronic: 2181,
+  },
+  {
+    name: 'Fri',
+    books: 2390,
+    clothes: 3800,
+    electronic: 2500,
+  },
+  {
+    name: 'Sat',
+    books: 3490,
+    clothes: 4300,
+    electronic: 2100,
+  },
+];
+
 export const userRows = [
   {
     id: 1,
