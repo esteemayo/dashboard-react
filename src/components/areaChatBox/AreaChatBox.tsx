6868466c4e3aca9d76dbffbfb215ab00bb916c19
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-19 21:00:44 +0100

    feat: add markup

--- src/components/areaChatBox/AreaChatBox.tsx
+++ src/components/areaChatBox/AreaChatBox.tsx
@@ -1,7 +1,62 @@
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+
+import { areaChartData } from '../../data';
+
 import './areaChatBox.scss';
 
 const AreaChatBox = () => {
-  return <div className='wrapper'>AreaChatBox</div>;
+  return (
+    <div className='wrapper'>
+      <h1>Revenue analytics</h1>
+      <div className='chart'>
+        <ResponsiveContainer width='100%' height='100%'>
+          <AreaChart
+            width={500}
+            height={400}
+            data={areaChartData}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <XAxis dataKey='name' />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type='monotone'
+              dataKey='books'
+              stackId='1'
+              stroke='#8884d8'
+              fill='#8884d8'
+            />
+            <Area
+              type='monotone'
+              dataKey='clothes'
+              stackId='1'
+              stroke='#82ca9d'
+              fill='#82ca9d'
+            />
+            <Area
+              type='electronic'
+              dataKey='amt'
+              stackId='1'
+              stroke='#ffc658'
+              fill='#ffc658'
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
 };
 
 export default AreaChatBox;
