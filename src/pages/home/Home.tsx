aa90235257668561e890ff1bac0791f650894ac0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-19 02:23:05 +0100

    fix: modify

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,7 @@ import './home.scss';
 
 const Home = () => {
   return (
-    <section className='container'>
+    <section className='home'>
       <div className='box box-1'>Box 1</div>
       <div className='box box-2'>Box 2</div>
       <div className='box box-3'>Box 3</div>
