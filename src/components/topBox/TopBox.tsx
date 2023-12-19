635b5f4eb6a8f21974425636bd566d373b31398c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-19 03:01:06 +0100

    feat: add markup

--- src/components/topBox/TopBox.tsx
+++ src/components/topBox/TopBox.tsx
@@ -1,7 +1,29 @@
+import { topDealUsers } from '../../data';
 import './topBox.scss';
 
 const TopBox = () => {
-  return <div className='wrapper'>TopBox</div>;
+  return (
+    <div className='wrapper'>
+      <h1>Top deals</h1>
+      <div className='list'>
+        {topDealUsers.map((item) => {
+          const { id, img, username, email, amount } = item;
+          return (
+            <div key={id} className='listItem'>
+              <div className='user'>
+                <img src={img} alt={username} />
+                <div className='userTexts'>
+                  <span className='username'>{username}</span>
+                  <span className='email'>{email}</span>
+                </div>
+              </div>
+              <span className='amount'>${amount}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TopBox;
