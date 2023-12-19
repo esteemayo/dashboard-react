253c166fbbc99d8dbebd3a0b26fb682c1f5b8b1e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-19 20:29:11 +0100

    feat: add markup

--- src/components/pieChartBox/PieChartBox.tsx
+++ src/components/pieChartBox/PieChartBox.tsx
@@ -1,7 +1,39 @@
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from 'recharts';
+
+import { pieChartData } from '../../data';
+
 import './pieChartBox.scss';
 
 const PieChartBox = () => {
-  return <div>PieChartBox</div>;
+  return (
+    <div className='wrapper'>
+      <h1>Leads by Source</h1>
+      <div className='chart'>
+        <ResponsiveContainer width='99%' height={300}>
+          <PieChart width={800} height={400}>
+            <Tooltip
+              contentStyle={{
+                backgroundColor: '#fff',
+                borderRadius: '0.5rem',
+              }}
+            />
+            <Pie
+              data={pieChartData}
+              innerRadius={'70%'}
+              outerRadius={'90%'}
+              paddingAngle={5}
+              dataKey='value'
+            >
+              {pieChartData.map((item) => {
+                const { name, color } = item;
+                return <Cell key={name} fill={color} />;
+              })}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
 };
 
 export default PieChartBox;
