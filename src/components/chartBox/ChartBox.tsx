a8d9ba6a8340d2249e4b15a2048ca9b52309b98b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-19 15:39:24 +0100

    fix: render component dynamically

--- src/components/chartBox/ChartBox.tsx
+++ src/components/chartBox/ChartBox.tsx
@@ -3,66 +3,31 @@ import { Link } from 'react-router-dom';
 import './chartBox.scss';
 import { LineChart, Line, ResponsiveContainer, Tooltip } from 'recharts';
 
-const ChartBox = () => {
-  const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
-
+const ChartBox = ({
+  icon,
+  title,
+  value,
+  chartData,
+  dataKey,
+  color,
+  percentage,
+}) => {
   return (
     <div className='chartBox'>
       <div className='boxInfo'>
         <div className='title'>
-          <img src='/user.svg' alt='' />
-          <span>Total users</span>
+          <img src={icon} alt='' />
+          <span>{title}</span>
         </div>
-        <h1>11.238</h1>
-        <Link to='/'>View all</Link>
+        <h1>{value}</h1>
+        <Link to='/' style={{ color }}>
+          View all
+        </Link>
       </div>
       <div className='chartInfo'>
         <div className='chart'>
           <ResponsiveContainer width='100%' height='100%'>
-            <LineChart width={300} height={100} data={data}>
+            <LineChart data={chartData}>
               <Tooltip
                 contentStyle={{
                   backgroundColor: 'transparent',
@@ -73,8 +38,8 @@ const ChartBox = () => {
               />
               <Line
                 type='monotone'
-                dataKey='pv'
-                stroke='#8884d8'
+                dataKey={dataKey}
+                stroke={color}
                 strokeWidth={2}
                 dot={false}
               />
@@ -82,7 +47,12 @@ const ChartBox = () => {
           </ResponsiveContainer>
         </div>
         <div className='texts'>
-          <span className='percentage'>45%</span>
+          <span
+            className='percentage'
+            style={{ color: percentage < 0 ? 'tomato' : 'limegreen' }}
+          >
+            {percentage}%
+          </span>
           <span className='duration'>this month</span>
         </div>
       </div>
