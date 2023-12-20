671dff8d68a798cc8adaeb7a362d96cca60e48d9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-20 19:41:42 +0100

    feat: add single user page component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Users from './pages/users/Users';
 import Layout from './components/layout/Layout';
 import Login from './pages/login/Login';
 import Product from './pages/product/Product';
+import User from './pages/user/User';
 
 import './styles/global.scss';
 
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
       {
         path: 'products',
         element: <Products />,
+      },
+      {
+        path: '/users/:id',
+        element: <User />,
       },
       {
         path: '/products/:id',
