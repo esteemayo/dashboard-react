4ca73d3bf1f2ab180a3992ca92e94cae3b52582e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-20 12:24:11 +0100

    feat: add Add component

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,16 +1,28 @@
-import { userColumns, userRows } from '../../data';
+import Add from '../../components/add/Add';
 import DataTable from '../../components/dataTable/DataTable';
+
+import { userColumns, userRows } from '../../data';
+import { useAddStore } from '../../hooks/useAddStore';
 
 import './users.scss';
 
 const Users = () => {
+  const isOpen = useAddStore((state) => state.isOpen);
+  const onOpen = useAddStore((state) => state.onOpen);
+  const onClose = useAddStore((state) => state.onClose);
   return (
     <div className='users'>
       <div className='info'>
         <h1>Users</h1>
-        <button>Add new user</button>
+        <button onClick={onOpen}>Add new user</button>
       </div>
       <DataTable columns={userColumns} rows={userRows} slug='users' />
+      <Add
+        slug='user'
+        columns={userColumns}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
     </div>
   );
 };
