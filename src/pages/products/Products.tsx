7cf6624fbcc070cf75e300a52b8fd85f83ee884a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-20 17:34:40 +0100

    feat: add markup

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,7 +1,31 @@
+import Add from '../../components/add/Add';
+import DataTable from '../../components/dataTable/DataTable';
+import { products, userColumns } from '../../data';
+
+import { useAddStore } from '../../hooks/useAddStore';
+
 import './products.scss';
 
 const Products = () => {
-  return <div>Products</div>;
+  const isOpen = useAddStore((state) => state.isOpen);
+  const onClose = useAddStore((state) => state.onClose);
+  const onOpen = useAddStore((state) => state.onOpen);
+
+  return (
+    <div className='products'>
+      <div className='info'>
+        <h1>Users</h1>
+        <button onClick={onOpen}>Add new product</button>
+      </div>
+      <DataTable columns={userColumns} rows={products} slug='users' />
+      <Add
+        slug='product'
+        columns={userColumns}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
+    </div>
+  );
 };
 
 export default Products;
