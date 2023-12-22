0d2dd0f3ef8b0f8a19dbd23007f43f385b719040
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-22 02:36:47 +0100

    fix: modify

--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -1,12 +1,35 @@
-import { FaTimes } from 'react-icons/fa';
 import { useCallback, useEffect, useState } from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { useMutation, QueryClient } from '@tanstack/react-query';
 
 import { AddProps } from '../../types';
 
 import './add.scss';
 
 const Add = ({ slug, columns, isOpen, onClose }: AddProps) => {
+  const queryClient = new QueryClient();
   const [showModal, setShowModal] = useState(isOpen);
+
+  const mutation = useMutation({
+    mutationFn: () => {
+      return fetch(`http://localhost:8800/api/v1/${slug}s`, {
+        method: 'POST',
+        body: JSON.stringify({
+          id: 111,
+          img: '',
+          lastName: 'Doe',
+          firstName: 'John',
+          email: 'jdoe@example.com',
+          phone: '123 456 789',
+          createdAt: '22.12.2023',
+          verified: true,
+        }),
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [`${slug}s`] });
+    },
+  });
 
   const handleClose = useCallback(
     (e: React.MouseEvent<HTMLElement>) => {
@@ -21,9 +44,13 @@ const Add = ({ slug, columns, isOpen, onClose }: AddProps) => {
     [onClose]
   );
 
-  const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  }, []);
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      mutation.mutate();
+    },
+    [mutation]
+  );
 
   useEffect(() => {
     setShowModal(isOpen);
