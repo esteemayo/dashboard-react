1b9eb14019df94504f2d197877a5f74c295f10e2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-26 00:58:26 +0100

    fix: modify

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 import './footer.scss';
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      <span>Esteem Admin</span>
+      <span>
+        <Link to='/'>Esteem Admin</Link>
+      </span>
       <span>© Esteem Admin dashboard</span>
     </footer>
   );
