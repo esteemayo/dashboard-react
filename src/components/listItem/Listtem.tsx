efb2822b6c0676b0fc955b79e77b7d6f3fd61403
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-28 00:31:04 +0100

    feat: add ListItemProps type annotation

--- src/components/listItem/Listtem.tsx
+++ src/components/listItem/Listtem.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 
+import { ListItemProps } from '../../types';
+
 import './listItem.scss';
 
-const ListItem = ({ url, icon, title }) => {
+const ListItem = ({ url, icon, title }: ListItemProps) => {
   return (
     <Link to={url} className='listItem'>
       <img src={icon} alt={title.toLowerCase()} />
