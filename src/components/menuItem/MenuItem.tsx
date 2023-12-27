9d71a49cfcaf6563ba02222cc3e3af850aee9a30
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-28 00:45:14 +0100

    feat: add markup

--- src/components/menuItem/MenuItem.tsx
+++ src/components/menuItem/MenuItem.tsx
@@ -1,7 +1,16 @@
+import ListItem from '../listItem/Listtem';
+
 import './menuItem.scss';
 
-const MenuItem = () => {
-  return <div>MenuItem</div>;
+const MenuItem = ({ title, listItems }) => {
+  return (
+    <div className='item'>
+      <span className='title'>{title}</span>
+      {listItems.map((item) => {
+        return <ListItem key={item.id} {...item} />;
+      })}
+    </div>
+  );
 };
 
 export default MenuItem;
