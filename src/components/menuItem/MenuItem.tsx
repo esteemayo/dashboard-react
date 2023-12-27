8028bc3c6a761ef67098a301660e7f1680de9b33
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-28 00:25:11 +0100

    feat: add markup

--- src/components/menuItem/MenuItem.tsx
+++ src/components/menuItem/MenuItem.tsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 import './menuItem.scss';
 
-const MenuItem = () => {
-  return <div>MenuItem</div>;
+const MenuItem = ({ url, icon, title }) => {
+  return (
+    <Link to={url} className='listItem'>
+      <img src={icon} alt={title.toLowerCase()} />
+      <span className='listItemTitle'>{title}</span>
+    </Link>
+  );
 };
 
 export default MenuItem;
