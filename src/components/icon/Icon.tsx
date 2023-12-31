98b60031964904a9067499e4088d571637e6d3f9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-31 23:42:05 +0100

    fix: modify

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,7 +1,5 @@
 import { IconProps } from '../../types';
 
-import './icon.scss';
-
 const Icon = ({ src, className }: IconProps) => {
   return <img src={src} alt='icon' className={className} />;
 };
