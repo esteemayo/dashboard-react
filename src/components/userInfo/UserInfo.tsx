f7edfef494f6903a09f25d6bc8eb4cabccae5f94
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-01 22:17:14 +0100

    feat: add markup

--- src/components/userInfo/UserInfo.tsx
+++ src/components/userInfo/UserInfo.tsx
@@ -1,5 +1,12 @@
+import Icon from '../icon/Icon';
+
 const UserInfo = () => {
-  return <div>UserInfo</div>;
+  return (
+    <div className='user'>
+      <Icon src='/user.jpeg' />
+      <span>Jane</span>
+    </div>
+  );
 };
 
 export default UserInfo;
