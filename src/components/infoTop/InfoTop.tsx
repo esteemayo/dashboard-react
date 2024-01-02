f94613227903d682629013010b3bb51fa34cef15
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-02 22:32:21 +0100

    feat: add InfoTopProps type annotation

--- src/components/infoTop/InfoTop.tsx
+++ src/components/infoTop/InfoTop.tsx
@@ -1,4 +1,6 @@
-const InfoTop = ({ img, title }) => {
+import { InfoTopProps } from '../../types';
+
+const InfoTop = ({ img, title }: InfoTopProps) => {
   return (
     <div className='info__top'>
       {img && <img src={img} alt={title} />}
