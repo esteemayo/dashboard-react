61db65fd5b85a7cdcfb74b74760278e0180aba27
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-02 22:23:56 +0100

    feat: add markup

--- src/components/infoDetails/InfoDetails.tsx
+++ src/components/infoDetails/InfoDetails.tsx
@@ -1,5 +1,16 @@
-const InfoDetails = () => {
-  return <div>InfoDetails</div>;
+const InfoDetails = ({ info }) => {
+  return (
+    <div className='info__details'>
+      {Object.entries(info).map(([key, value]) => {
+        return (
+          <div key={key} className='item'>
+            <span className='item__title'>{key}:</span>
+            <span className='item__value'>{value}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default InfoDetails;
