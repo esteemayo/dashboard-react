a9f06ce19affad394a7c470191aa1f3ac83672d1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-02 22:30:55 +0100

    feat: add markup

--- src/components/infoTop/InfoTop.tsx
+++ src/components/infoTop/InfoTop.tsx
@@ -1,5 +1,11 @@
-const InfoTop = () => {
-  return <div>InfoTop</div>;
+const InfoTop = ({ img, title }) => {
+  return (
+    <div className='info__top'>
+      {img && <img src={img} alt={title} />}
+      <h1>{title}</h1>
+      <button type='button'>Update</button>
+    </div>
+  );
 };
 
 export default InfoTop;
