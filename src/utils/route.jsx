368f092a0a70dd6cc06e7702026f46b443061b8c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-10 21:21:25 +0100

    fix: modify

--- src/utils/route.jsx
+++ src/utils/route.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Home from '../pages/home/Home';
-import Products from '../pages/products/Products';
-import Users from '../pages/users/Users';
-import Layout from '../components/layout/Layout';
 import Login from '../pages/login/Login';
-import Product from '../pages/product/Product';
 import User from '../pages/user/User';
+import Users from '../pages/users/Users';
+
+import Product from '../pages/product/Product';
+import Products from '../pages/products/Products';
+
+import Layout from '../components/layout/Layout';
 
 export const router = createBrowserRouter([
   {
