0c2fa72e3edc2cc09b70de966c642f2d6ac1f169
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-10 21:19:45 +0100

    fix: refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,8 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
-import Home from './pages/home/Home';
-import Products from './pages/products/Products';
-import Users from './pages/users/Users';
-import Layout from './components/layout/Layout';
-import Login from './pages/login/Login';
-import Product from './pages/product/Product';
-import User from './pages/user/User';
+import router from './utils/route';
 
 import './styles/global.scss';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: 'users',
-        element: <Users />,
-      },
-      {
-        path: 'products',
-        element: <Products />,
-      },
-      {
-        path: '/users/:id',
-        element: <User />,
-      },
-      {
-        path: '/products/:id',
-        element: <Product />,
-      },
-    ],
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-]);
 
 function App() {
   return <RouterProvider router={router} />;
